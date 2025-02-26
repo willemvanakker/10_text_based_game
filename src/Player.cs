6ba6499cd6e380ddf2a02076865b7c80f7277825
Willem Van Akker <willem.van.akker@gmail.com>
2025-02-26 12:38:39 +0100

    player health added

--- src/Player.cs
+++ src/Player.cs
@@ -2,10 +2,36 @@ class Player
 {
     // Auto property
     public Room CurrentRoom { get; set; }
+    private int health;
 
     // Constructor
     public Player()
     {
         CurrentRoom = null;
+        health = 100;
+    }
+
+    // Damage player
+    public void Damage(int amount)
+    {
+        health -= amount;
+    }
+
+    // Heal player
+    public void Heal(int amount)
+    {
+        health += amount;
+    }
+
+    // Get player health
+    public void getHealth()
+    {
+        return health;
+    }
+
+    // Check if player is alive
+    public void isAlive()
+    {
+        return health > 0;
     }
 }
