3469c70e149e4f0138f4686586762c4e0307c2be
Willem Van Akker <willem.van.akker@gmail.com>
2025-02-26 12:25:43 +0100

    added up and down command

--- src/Command.cs
+++ src/Command.cs
@@ -1,27 +1,25 @@
 class Command
 {
-	public string CommandWord { get; init; }
-	public string SecondWord { get; init; }
-	
-	// Create a command object. First and second word must be supplied, but
-	// either one (or both) can be null. See Parser.GetCommand()
-	public Command(string first, string second)
-	{
-		CommandWord = first;
-		SecondWord = second;
-	}
+    public string CommandWord { get; init; }
+    public string SecondWord { get; init; }
 
-	
-	// Return true if this command was not understood.
-	public bool IsUnknown()
-	{
-		return CommandWord == null;
-	}
+    // Create a command object. First and second word must be supplied, but
+    // either one (or both) can be null. See Parser.GetCommand()
+    public Command(string first, string second)
+    {
+        CommandWord = first;
+        SecondWord = second;
+    }
 
-	
-	// Return true if the command has a second word.
-	public bool HasSecondWord()
-	{
-		return SecondWord != null;
-	}
+    // Return true if this command was not understood.
+    public bool IsUnknown()
+    {
+        return CommandWord == null;
+    }
+
+    // Return true if the command has a second word.
+    public bool HasSecondWord()
+    {
+        return SecondWord != null;
+    }
 }
